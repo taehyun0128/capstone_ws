ee1f841adea6480d60f9f52cba0e4ddc50f6e8db
taehyun0128 <xogusrla01@gmail.com>
2026-05-04 21:47:35 +0900

    add stvl

--- src/rko_lio/cmake/dependencies/json/nlohmann_json.cmake
+++
@@ -1,5 +0,0 @@
-FetchContent_Declare(
-  nlohmann_json
-  URL https://github.com/nlohmann/json/archive/refs/tags/v3.12.0.tar.gz SYSTEM
-      EXCLUDE_FROM_ALL OVERRIDE_FIND_PACKAGE)
-FetchContent_MakeAvailable(nlohmann_json)
--- src/rko_lio/python/rko_lio/dataloaders/__init__.py
+++
@@ -1,1 +0,0 @@
-from .get_dataloader import available_dataloaders, get_dataloader
